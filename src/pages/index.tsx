b07d6837836d20c81afef741d48bf5414ea6e5d0
Cesar Almendarez <cesaralmendarez@AlexandersAir2.ddnszone.calstatela.edu>
2023-10-09 22:37:02 -0700

    vercel deployment test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
     return (
         <div>
-            <h1>888 Notary Services</h1>
+            <h1>888 Notary Services - Redeployment</h1>
         </div>
     )
 }
